8d07e5ab5f0da0ddc577578640a57ed7b8fde733
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-03-30 20:08:04 +0100

    Create answers.js

---
+++ javascript/answers.js
@@ -0,0 +1,19 @@
+
+try {
+    alert('hi');
+} catch (error) {
+    console.log(error)
+}
+/* const btn = document.getElementById('#answerButton');
+
+btn.onclick = function () {
+    console.log('clicked')
+}
+
+const checkAnswer = function (usersAnswer, correctAnswer) {
+    if (usersAnswer != correctAnswer) {
+        console.log('wrong answer')
+    } else if (usersAnswer == correctAnswer) {
+        console.log('correct answer')
+    }
+}  */
