6d477569ade5d2172b0119ece04b32bad4fa6a8a
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-05-01 18:25:35 +0100

    Subtraction answers and questions file

---
+++ seeds/subtractionQuestionAnswers.js
@@ -0,0 +1,109 @@
+module.exports.subtractionQuestions = [
+    {
+        id: 1,
+        question: 'What is 30 - 12?'
+    },
+    {
+        id: 2,
+        question: 'Decrease 13 from 40'
+    },
+    {
+        id: 3,
+        question: 'What is the difference between 6.3 and 2.4?'
+    },
+    {
+        id: 4,
+        question: 'Decrease 10.2 by 3.8'
+    }
+]
+
+module.exports.subtractionAnswers = [
+    {
+        id: 1,
+        correctAnswer: 18,
+        answers: [
+            {
+                id: 1,
+                answer: 17
+            },
+            {
+                id: 2,
+                answer: 18
+            },
+            {
+                id: 3,
+                answer: 19
+            },
+            {
+                id: 4,
+                answer: 16
+            },
+        ]
+    },
+    {
+        id: 2,
+        correctAnswer: 27,
+        answers: [
+            {
+                id: 1,
+                answer: 37
+            },
+            {
+                id: 2,
+                answer: 26
+            },
+            {
+                id: 3,
+                answer: 27
+            },
+            {
+                id: 4,
+                answer: 28
+            },
+        ]
+    },
+    {
+        id: 3,
+        correctAnswer: 3.9,
+        answers: [
+            {
+                id: 1,
+                answer: 2.9
+            },
+            {
+                id: 2,
+                answer: 3.9
+            },
+            {
+                id: 3,
+                answer: 4
+            },
+            {
+                id: 4,
+                answer: 4.1
+            },
+        ]
+    },
+    {
+        id: 4,
+        correctAnswer: 6.4,
+        answers: [
+            {
+                id: 1,
+                answer: 6.5
+            },
+            {
+                id: 2,
+                answer: 7.4
+            },
+            {
+                id: 3,
+                answer: 6.4
+            },
+            {
+                id: 4,
+                answer: 6.2
+            },
+        ]
+    },
+]
