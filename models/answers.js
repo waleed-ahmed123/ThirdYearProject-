68228e3414989e20b7bf3376eb4096e10177710e
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-05-04 11:05:21 +0100

    Adding the multiplication and division model schemas for mongoose

--- models/answers.js
+++ models/answers.js
@@ -19,5 +19,25 @@ const subtractionAnswerSchema = new Schema({
     }]
 })
 
+const multiplicationAnswerSchema = new Schema({
+    id: Number,
+    correctAnswer: Number,
+    answers: [{
+        id: Number,
+        answer: Number
+    }]
+})
+
+const divisionAnswerSchema = new Schema({
+    id: Number,
+    correctAnswer: Number,
+    answers: [{
+        id: Number,
+        answer: Number
+    }]
+})
+
 module.exports.AdditionAnswers = mongoose.model('AdditionAnswers', additionAnswerSchema);
 module.exports.SubtractionAnswers = mongoose.model('SubtractionAnswers', subtractionAnswerSchema);
+module.exports.MultiplicationAnswers = mongoose.model('MultiplicationAnswers', multiplicationAnswerSchema);
+module.exports.DivisionAnswers = mongoose.model('DivisionAnswers', divisionAnswerSchema);
